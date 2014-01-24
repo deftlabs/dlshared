a17ef899e6443c7d193c811c6fa8fbcdc19ae83b
Ryan <rgnitz@gmail.com>
2014-01-24 10:25:02 -0500

    fixed copyright

--- src/deftlabs.com/net/http/http_server.go
+++ src/deftlabs.com/net/http/http_server.go
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright 2013 Deft Labs
+ * (C) Copyright 2013, Deft Labs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
