c157a841897968f5fcff70ce9bb514e058481644
Ryan <rgnitz@gmail.com>
2014-01-24 16:35:56 -0500

    working on it

--- src/deftlabs.com/ds/mongodb.go
+++ src/deftlabs.com/ds/mongodb.go
@@ -5,21 +5,54 @@
 package deftlabsds
 
 import (
+	"fmt"
 	"time"
 	"labix.org/v2/mgo"
 	"deftlabs.com/log"
 	"deftlabs.com/kernel"
 )
 
+// Create a new MongoDb component from a configuration path. The path passed must be in the following format.
+//
+// mongodb: {
+//     configDb: {
+//         mongoDbUrl: "mongodb://localhost:27017/test",
+//         safeMode: 0,
+//         dialTimeoutInMs: 3000,
+//         socketTimeoutInMs: 3000,
+//         syncTimeoutInMs: 3000,
+//         cursorTimeoutInMs: 30000,
+//     }
+// }
+//
+// The configPath for this component would be "mongodb.configDb". The path can be any arbitrary set of nested
+// json documents (json path). If the path is incorrect, the Start() method will panic when called by the kernel.
+//
+// All of the params above must be present or
+// the Start method will panic. If the componentId or configPath param is nil or empty,
+// this method will panic.
+func NewMongoDbFromConfigPath(componentId, configPath string) *MongoDb {
+
+	if len(componentId) == 0 {
+		panic("When calling NewMongoDbFromConfigPath you must pass in a non-empty componentId param")
+	}
+
+	if len(configPath) == 0 {
+		panic("When calling NewMongoDbFromConfigPath you must pass in a non-empty configPath param")
+	}
+
+	return &MongoDb{ componentId : componentId, configPath : configPath }
+}
+
 // Create a new MongoDb component. This method will panic if either of the params are nil or len == 0.
-func NewMongoDb(componentId, mongoDbUrl string, safeMode, dialTimeoutInMs, socketTimeoutInMs, syncTimeoutInMs, cursorTimeoutInMs  int) *MongoDb {
+func NewMongoDb(componentId, mongoDbUrl string, safeMode, dialTimeoutInMs, socketTimeoutInMs, syncTimeoutInMs, cursorTimeoutInMs int) *MongoDb {
 
 	if len(componentId) == 0 {
 		panic("When calling NewMongoDb you must pass in a non-empty component id")
 	}
 
 	if len(mongoDbUrl) == 0 {
-		panic("When calling NewMongoDb you must pass in a valid MongoDb url")
+		panic("When calling NewMongoDb you must pass in a non-empty MongoDb url")
 	}
 
 	return &MongoDb{
@@ -34,7 +67,11 @@ func NewMongoDb(componentId, mongoDbUrl string, safeMode, dialTimeoutInMs, socke
 }
 
 type MongoDb struct {
+	kernel *deftlabskernel.Kernel
 	slogger.Logger
+
+	configPath string
+
 	componentId string
 	mongoDbUrl string
 	safeMode int
@@ -61,13 +98,48 @@ func (self *MongoDb) SessionClone() *mgo.Session { return self.session.Clone() }
 func (self *MongoDb) SessionCopy() *mgo.Session { return self.session.Clone() }
 
 func (self *MongoDb) Start(kernel *deftlabskernel.Kernel) error {
+	self.kernel = kernel
 	self.Logger = kernel.Logger
 
 	var err error
 
-	self.session, err = mgo.DialWithTimeout(self.mongoDbUrl, time.Duration(self.dialTimeoutInMs) * time.Millisecond)
+	// This is a configuration based creation. Load the config data first.
+	if len(self.configPath) > 0 {
+		self.mongoDbUrl = self.kernel.Configuration.String(fmt.Sprintf("%s.%s", self.configPath, "mongoDbUrl"), "")
+		self.safeMode = self.kernel.Configuration.Int(fmt.Sprintf("%s.%s", self.configPath, "safeMode"), -1)
+		self.dialTimeoutInMs = self.kernel.Configuration.Int(fmt.Sprintf("%s.%s", self.configPath, "dialTimeoutInMs"), -1)
+		self.socketTimeoutInMs = self.kernel.Configuration.Int(fmt.Sprintf("%s.%s", self.configPath, "socketTimeoutInMs"), -1)
+		self.syncTimeoutInMs = self.kernel.Configuration.Int(fmt.Sprintf("%s.%s", self.configPath, "syncTimeoutInMs"), -1)
+		self.cursorTimeoutInMs = self.kernel.Configuration.Int(fmt.Sprintf("%s.%s", self.configPath, "cursorTimeoutInMs"), -1)
+	}
 
-	if err != nil {
+	// Validate the params
+	if len(self.mongoDbUrl) == 0 {
+		panic(fmt.Sprintf("In MongoDb - mongoDbUrl is not set - componentId: %s", self.componentId))
+	}
+
+	if self.dialTimeoutInMs < 0 {
+		panic(fmt.Sprintf("In MongoDb - dialTimeoutInMs is invalid - value: %d - componentId: %s", self.dialTimeoutInMs, self.componentId))
+	}
+
+	if self.socketTimeoutInMs < 0 {
+		panic(fmt.Sprintf("In MongoDb - socketTimeoutInMs is invalid - value: %d - componentId: %s", self.socketTimeoutInMs, self.componentId))
+	}
+
+	if self.syncTimeoutInMs < 0 {
+		panic(fmt.Sprintf("In MongoDb - syncTimeoutInMs is invalid - value: %d - componentId: %s", self.syncTimeoutInMs, self.componentId))
+	}
+
+	if self.cursorTimeoutInMs < 0 {
+		panic(fmt.Sprintf("In MongoDb - cursorTimeoutInMs is invalid - value: %d - componentId: %s", self.cursorTimeoutInMs, self.componentId))
+	}
+
+	if self.safeMode < 0  || self.safeMode > 2 {
+		panic(fmt.Sprintf("In MongoDb - safeMode is invalid - value: %d - componentId: %s", self.safeMode, self.componentId))
+	}
+
+	// Create the session.
+	if self.session, err = mgo.DialWithTimeout(self.mongoDbUrl, time.Duration(self.dialTimeoutInMs) * time.Millisecond); err != nil {
 		return slogger.NewStackError("Unable to init MongoDb session - component: %s - mongodbUrl: %s", self.componentId, self.mongoDbUrl)
 	}
 
@@ -84,6 +156,7 @@ func (self *MongoDb) Start(kernel *deftlabskernel.Kernel) error {
 	return nil
 }
 
+// Stop the component. This will close the base session.
 func (self *MongoDb) Stop(kernel *deftlabskernel.Kernel) error {
 
 	if self.session != nil {
