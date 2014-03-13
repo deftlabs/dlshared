a50ba3e5425ced520ea0e512e74b989ac3b8e3ae
Ryan <rgnitz@gmail.com>
2014-03-13 17:30:18 -0400

    initial load

---
+++ http_test_utils.go
@@ -0,0 +1,49 @@
+/**
+ * (C) Copyright 2014, Deft Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dlshared
+
+import "net/http"
+
+type RecordingResponseWriter struct {
+	header http.Header
+	HeaderCode int
+	Data []byte
+}
+
+func (self *RecordingResponseWriter) reset() {
+	self.header = make(map[string][]string)
+	self.HeaderCode = 0
+	self.Data = nil
+}
+
+func (self *RecordingResponseWriter) Header() http.Header {
+	return self.header
+}
+
+func (self *RecordingResponseWriter) Write(data []byte) (int, error) {
+	self.Data = append(self.Data, data...)
+	return len(data), nil
+}
+
+func NewRecordingResponseWriter() *RecordingResponseWriter {
+	return &RecordingResponseWriter{ header : make(map[string][]string) }
+}
+
+func (self *RecordingResponseWriter) WriteHeader(code int) {
+	self.HeaderCode = code
+}
+
