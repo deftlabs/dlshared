74e7d72be33aa0aa5cb9e21de696988acfa221d0
Ryan <rgnitz@gmail.com>
2014-03-17 11:07:56 -0400

    added push conv method.

--- data_source.go
+++ data_source.go
@@ -100,6 +100,22 @@ func (self *DataSource) SetFieldsSafe(query interface{}, fieldsDoc interface{}) 
 	return nil
 }
 
+// Add to set call using a "safe" operation. If this is a standalone mongo or a mongos, it will use: WMode: "majority".
+// If this is a standalone mongo, it will use: w: 1
+func (self *DataSource) PushSafe(query interface{}, fieldsDoc interface{}) error {
+	session := self.SessionClone()
+	defer session.Close()
+
+	session.SetSafe(self.Mongo.DefaultSafe)
+
+	update := &bson.M{ "$push": fieldsDoc }
+
+	if err := self.RemoveNotFoundErr(session.DB(self.DbName).C(self.CollectionName).Update(query, update)); err != nil {
+		return NewStackError("Unable to PushSafe - db: %s - collection: %s - error: %v", self.DbName, self.CollectionName, err)
+	}
+
+	return nil
+}
 
 // Set a property using a "safe" operation. If this is a standalone mongo or a mongos, it will use: WMode: "majority".
 // If this is a standalone mongo, it will use: w: 1
