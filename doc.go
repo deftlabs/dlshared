2426780a063da9b0b44f79655ec7b72dd725a947
Ryan <rgnitz@gmail.com>
2014-04-02 11:36:01 -0400

    initial load.

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package dlshared provides a set of reusable tools for Go application development.
+//
+// Details about the mgo project (pronounced as "mango") are found
+// in its web page:
+//
+//     https://github.com/deftlabs/dlshared
+//
+//
+// For more information, see the documentation below.
+//
+package dlshared
