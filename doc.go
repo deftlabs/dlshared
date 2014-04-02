6150f391304ddc6215937759398afa8019c3c409
Ryan <rgnitz@gmail.com>
2014-04-02 11:44:49 -0400

    flushed out info

--- doc.go
+++ doc.go
@@ -1,9 +1,19 @@
 // Package dlshared provides a set of reusable tools for Go application development.
 //
-// Details about the mgo project (pronounced as "mango") are found
-// in its web page:
+// Details about the dlshared library are found its project page:
 //
 //     https://github.com/deftlabs/dlshared
+//
+// External libraries:
+//
+//     github.com/mreiferson/go-httpclient
+// 	   labix.org/v2/mgo
+//     github.com/daviddengcn/go-ljson-conf
+//     github.com/gorilla/mux
+//     code.google.com/p/go.crypto/bcrypt
+//     github.com/nranchev/go-libGeoIP
+//
+// To initialize the external libraries, run: `make init.libs`
 //
 //
 // For more information, see the documentation below.
