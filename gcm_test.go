02a185d60facd8d08692c8957f06bf82a965a2bb
Ryan <rgnitz@gmail.com>
2014-05-06 12:32:32 -0400

    changed to handle updated gcm logic.

--- gcm_test.go
+++ gcm_test.go
@@ -16,12 +16,31 @@
 
 package dlshared
 
-import "testing"
+import (
+	"sync"
+	"testing"
+	"encoding/json"
+)
 
 // Test the google cloud messaging service.
 func TestGcm(t *testing.T) {
 
-	svc := NewGoogleCloudMessagingSvc("gcm", nil)
+	mockResponse := testCreateGoogleCloudMsgResponse(100, 1, 0, 0)
+	testAddGoogleCloudMsgResponseResult(mockResponse, "someMessageId", nadaStr, nadaStr)
+
+	data, err := json.Marshal(mockResponse)
+	if err != nil { t.Errorf("TestGcm json encode mock response broken - err: %v", err); return }
+
+	httpClient := NewHttpRequestClientMock()
+	httpClient.(*HttpRequestClientMock).AddMock("https://android.googleapis.com/gcm/send", &HttpRequestClientMockResponse{
+		HttpStatusCode: 200,
+		Data: data,
+	})
+
+	requestChannel := make(chan interface{})
+	responseChannel := make(chan interface{})
+
+	svc := NewGoogleCloudMessagingSvc("gcm", httpClient, requestChannel, responseChannel)
 
 	kernel, err := baseTestStartKernel("gcmTest", func(kernel *Kernel) {
 		kernel.AddComponentWithStartStopMethods("GoogleCloudMessagingSvc", svc, "Start", "Stop")
@@ -29,12 +48,55 @@ func TestGcm(t *testing.T) {
 
 	if err != nil { t.Errorf("TestGcm start kernel is broken: %v", err); return }
 
-	// TODO: Simulate
+	msgSendCount := 100000
+	msgReceivedCount := 0
 
-	svc.AddResponseHandler(func(response *GoogleCloudMsgResponse) {
+	var waitGroup sync.WaitGroup
 
-	})
+	go func() {
+		waitGroup.Add(1)
+		defer waitGroup.Done()
+		for {
+			msg := <- responseChannel
+			if msg == nil { t.Errorf("TestGcm is broken - response message is nil") }
+			msgReceivedCount++
+			if msgReceivedCount == msgSendCount { return }
+		}
+	}()
+
+	for idx := 0; idx < msgSendCount; idx++ {
+		requestChannel <- &GoogleCloudMsg{
+			RegistrationIds: []string { "someRegistrationId" },
+			CollapseKey: "someCollapseKey",
+			DelayWhileIdle: true,
+			TimeToLive: 300,
+			RestrictedPackageName: "somePackageName",
+			DryRun: false,
+			Data: map[string]interface{} { "someKey": "someValue" },
+		}
+	}
+
+	waitGroup.Wait()
+
+	close(requestChannel)
 
 	if err := kernel.Stop(); err != nil { t.Errorf("TestGcm stop kernel is broken:", err) }
+}
+
+func testCreateGoogleCloudMsgResponse(multicastId, success, failure, canonicalIds float64) *GoogleCloudMsgResponse {
+	return &GoogleCloudMsgResponse {
+		MulticastId: multicastId,
+		Success: success,
+		Failure: failure,
+		CanonicalIds: canonicalIds,
+	}
+}
+
+func testAddGoogleCloudMsgResponseResult(response *GoogleCloudMsgResponse, messageId, registrationId, err string) {
+	response.Results = append(response.Results, &GoogleCloudMsgResponseResult{
+		MessageId: messageId,
+		RegistrationId: registrationId,
+		Error: err,
+	})
 }
 
